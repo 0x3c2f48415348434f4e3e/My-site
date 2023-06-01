08e94120090ef6ce21547b915cfadbba3d019f5d
Benjamin Enadeba <qg002757@student.reading.ac.uk>
2023-06-02 00:54:49 +0100

    Javascript

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data:function(){
+        return{
+            username:"HASHcon",
+            GitHub_img:"./icons/github.png",
+        };
+    }
+})
+
+app.mount('#app')
